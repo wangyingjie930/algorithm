9d24b81113e1d72e8d93084568fb5be56054368f
wangyingjie <930055912@qq.com>
2023-02-22 11:06:02 +0800

    feat 求根平方

--- 算法思想/二分/求根平方/sqrt.go
+++ 算法思想/二分/求根平方/sqrt.go
@@ -1,22 +1,19 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println(mySqrt(457575))
-}
+package 求根平方
 
 func mySqrt(x int) int {
 	l, r := 0, x
 	ans := -1
 	for l <= r {
-		mid := l + (r - l) / 2
-		if mid * mid <= x {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
 			ans = mid
 			l = mid + 1
 		} else {
 			r = mid - 1
 		}
+	}
+	if ans*ans != x {
+		return -1
 	}
 	return ans
 }
---
+++ 算法思想/二分/求根平方/sqrt_test.go
@@ -0,0 +1,32 @@
+/**
+  @author: wangyingjie
+  @since: 2023/2/22
+  @desc: //TODO
+**/
+
+package 求根平方
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "case1", args: args{x: 16}, want: 4},
+		{name: "case1", args: args{x: 20164}, want: 142},
+		{name: "case1", args: args{x: 981130329}, want: 31323},
+		{name: "case1", args: args{x: 10001}, want: -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mySqrt(tt.args.x); got != tt.want {
+				t.Errorf("mySqrt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
