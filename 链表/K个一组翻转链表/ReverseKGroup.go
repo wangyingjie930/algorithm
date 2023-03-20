f0dc4c8969828a3c29e180830d47dc8b776bcce1
wangyingjie <930055912@qq.com>
2023-03-21 02:16:00 +0800

    feat 链表：递归/迭代 K 个一组翻转链表

--- 链表/K个一组翻转链表/ReverseKGroup.go
+++ 链表/K个一组翻转链表/ReverseKGroup.go
@@ -3,68 +3,70 @@
   @since: 2023/2/8
   @desc: K 个一组翻转链表
   @see: https://leetcode.cn/problems/reverse-nodes-in-k-group/
+  掌握程度: 边界处理掌握不够 递归+迭代
 **/
 
 package K个一组翻转链表
 
-import (
-	SingList "algorithm/链表/单链表"
-	"errors"
-)
+import List "algorithm/链表/单链表"
 
-func reverseKGroup(head *SingList.Node, k int) *SingList.Node {
-	// 对第一组进行翻转以获得新的头指针
-	h, cur, err := reverseList(head, k)
-	if err != nil {
-		return head
-	}
+func reverseKGroup(head *List.Node, k int) *List.Node {
+	tailNext := head
 
-	head = h
-	for cur.Next != nil {
-		//对下一组进行翻转, 返回头指针, 尾指针
-		h, t, err := reverseList(cur.Next, k)
-		if err != nil {
-			break
+	for i := 0; i < k; i++ {
+		if tailNext == nil {
+			//剩下不满足的无需反转直接返回
+			return head
 		}
-		//将前面一组的最后一个节点指向下一个组翻转后的的头节点
-		cur.Next = h
-		cur = t
+		tailNext = tailNext.Next
 	}
-	return head
+
+	reverseList := reverse(head, tailNext)
+	newList := reverseKGroup(tailNext, k)
+	head.Next = newList
+	return reverseList
 }
 
-// reverseList
-//  @Description: 进行链表的翻转操作
-//  @param start
-//  @param count
-//  @return *SingList.Node
-func reverseList(start *SingList.Node, count int) (head, tail *SingList.Node, err error) {
-	if !canReverse(start, count) {
-		return nil, nil, errors.New("不能进行链表的翻转操作")
+// reverse
+//  @Description: 普通链表反转
+//  @param head
+//  @param tailNext 注意是尾结点的下一个节点
+//  @return *List.Node
+func reverse(head, tailNext *List.Node) *List.Node {
+	var pre *List.Node
+	for head != tailNext {
+		tmp := head.Next
+		head.Next = pre
+		pre = head
+		head = tmp
 	}
-
-	cur := start
-	var p *SingList.Node
-	for i := count; i >= 1; i-- {
-		next := cur.Next
-		cur.Next = p
-		p = cur
-		cur = next
-	}
-	// 反转后, 之前的头结点为反转后的尾结点, 需要将start节点的下一个节点指向下一组还没反转的头节点
-	start.Next = cur
-	return p, start, nil
+	return pre
 }
 
-// canReverse
-//  @Description: 判断是否能进行翻转, 如果数量不满足不给反转
-//  @param start
-//  @param count
-//  @return bool
-func canReverse(start *SingList.Node, count int) bool {
-	i := count
-	for p := start; i >= 1 && p != nil; i-- {
-		p = p.Next
+// reverseKGroupIteration
+//  @Description: 使用迭代法实现
+//  @param head
+//  @param k
+//  @return *List.Node
+func reverseKGroupIteration(head *List.Node, k int) *List.Node {
+	dummyNode := &List.Node{Next: head} //关键: 因为中间反转后, 需要有一个pre和next来进行串联
+	pre := dummyNode                    //这里就是需要串联的开头
+	for head != nil {
+		tail := head
+		for i := 1; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return dummyNode.Next
+			}
+		}
+		tailNext := tail.Next
+
+		reverseList := reverse(head, tailNext)
+		pre.Next = reverseList //头串联
+		head.Next = tailNext   //尾串联
+
+		pre = head
+		head = pre.Next
 	}
-	return i == 0
+	return dummyNode.Next
 }
--- 链表/K个一组翻转链表/ReverseKGroup_test.go
+++ 链表/K个一组翻转链表/ReverseKGroup_test.go
@@ -7,24 +7,60 @@
 package K个一组翻转链表
 
 import (
-	SingleList "algorithm/链表/单链表"
-	"fmt"
+	List "algorithm/链表/单链表"
 	"testing"
 )
 
 func Test_reverseKGroup(t *testing.T) {
-	list := SingleList.NewSingleList()
-	list.AddNode(1)
-	list.AddNode(2)
-	list.AddNode(3)
-	list.AddNode(4)
-	list.AddNode(5)
-	list.AddNode(6)
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "case1", args: args{nums: []int{1, 2, 3, 4, 5}, k: 2}},
+		{name: "case1", args: args{nums: []int{1, 2, 3, 4, 5}, k: 3}},
+		{name: "case1", args: args{nums: []int{}, k: 3}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list1 := List.NewSingleList()
+			for _, v := range tt.args.nums {
+				list1.AddNode(v)
+			}
+			list1.Traverse()
 
-	fmt.Print("before: ")
-	list.Traverse()
-	fmt.Print("\n")
+			list1.Head = reverseKGroup(list1.Head, tt.args.k)
+			list1.Traverse()
+		})
+	}
+}
 
-	list.Head = reverseKGroup(list.Head, 2)
-	list.Traverse()
+func Test_reverseKGroupIteration(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "case1", args: args{nums: []int{1, 2, 3, 4, 5}, k: 2}},
+		{name: "case1", args: args{nums: []int{1, 2, 3, 4, 5}, k: 3}},
+		{name: "case1", args: args{nums: []int{}, k: 3}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list1 := List.NewSingleList()
+			for _, v := range tt.args.nums {
+				list1.AddNode(v)
+			}
+			list1.Traverse()
+
+			list1.Head = reverseKGroupIteration(list1.Head, tt.args.k)
+			list1.Traverse()
+		})
+	}
 }
