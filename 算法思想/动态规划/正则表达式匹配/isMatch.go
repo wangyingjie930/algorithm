c2e65350a8c7083eabc820560a35174f4cf0afaa
wangyingjie <yingjiewang@tencentmusic.com>
2023-05-21 13:25:33 +0800

    feat 【动态规划】正则表达是匹配

--- 算法思想/动态规划/正则表达式匹配/isMatch.go
+++ 算法思想/动态规划/正则表达式匹配/isMatch.go
@@ -2,19 +2,36 @@
   @author: wangyingjie
   @since: 2023/5/18
   @desc: https://leetcode.cn/problems/regular-expression-matching/
+  @tag: unskilled
 **/
 
 package isMatch
 
+//isMatch
+//@Description:https://labuladong.unskilledgithub.io/algo/di-er-zhan-a01c6/yong-dong--63ceb/jing-dian--8d516/
+//@param s string
+//@param p string
+//@return bool
 func isMatch(s string, p string) bool {
 	return helper(s, 0, p, 0)
 }
 
 func helper(s string, i int, p string, j int) bool {
-	if i == len(s)-1 && j == len(p)-1 {
+	m, n := len(s), len(p)
+	// base case
+	if j == n {
+		return i == m
+	}
+	if i == m {
+		if (n-j)%2 == 1 {
+			return false
+		}
+		for ; j+1 < n; j += 2 {
+			if p[j+1] != '*' {
+				return false
+			}
+		}
 		return true
-	} else if i > len(s)-1 || j > len(p)-1 {
-		return false
 	}
 
 	if s[i] == p[j] || p[j] == '.' {
@@ -33,4 +50,50 @@ func helper(s string, i int, p string, j int) bool {
 			return false
 		}
 	}
+}
+
+//isMatchDp
+//@Description:https://leetcode.cn/problems/regular-expression-matching/solutions/296114/shou-hui-tu-jie-wo-tai-nan-liao-by-hyj8/
+//@param s string
+//@param p string
+//@return bool
+func isMatchDp(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+	//dp[i][j]: 表示s的前i个字符与p的前j个字符是否是匹配的
+	//并且是从后往前进行匹配的
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, n+1)
+	}
+
+	dp[0][0] = true
+	for j := 1; j <= n; j++ {
+		if p[j-1] == '*' {
+			//当s为空串的时候, 且当前的p字符(下标为j-1)为'*', 则跳过'*'及'*'前面的字符从下标为 j-1-2进行匹配
+			dp[0][j] = dp[0][j-2]
+		}
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if s[i-1] == p[j-1] || p[j-1] == '.' {
+				//当前s和p的字符是相同的情况
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				//不相同则看看比较的p是否为'*'
+				if p[j-1] != '*' {
+					//不是'*'直接判断不匹配
+					dp[i][j] = false
+				} else {
+					//是'*', 还要看看'*'前面的字符是什么, 详细链接有画图
+					if s[i-1] == p[j-2] || p[j-2] == '.' {
+						dp[i][j] = dp[i][j-2] || dp[i-1][j-2] || dp[i-1][j]
+					} else {
+						dp[i][j] = dp[i][j-2]
+					}
+				}
+			}
+		}
+	}
+	return dp[m][n]
 }
--- 算法思想/动态规划/正则表达式匹配/isMatch_test.go
+++ 算法思想/动态规划/正则表达式匹配/isMatch_test.go
@@ -26,11 +26,31 @@ func Test_isMatch(t *testing.T) {
 			s: "aaa",
 			p: "ab*aa",
 		}, want: true},
+		{name: "case1", args: args{
+			s: "ab",
+			p: ".*",
+		}, want: true},
+		{name: "case1", args: args{
+			s: "aa",
+			p: "a*",
+		}, want: true},
+		{name: "case1", args: args{
+			s: "ab",
+			p: ".*c",
+		}, want: false},
+		{name: "case1", args: args{
+			s: "aab",
+			p: "c*a*b",
+		}, want: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
 				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+
+			if got := isMatchDp(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatchDp() = %v, want %v", got, tt.want)
 			}
 		})
 	}
